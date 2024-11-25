3f67ae0eeb0612887263ba1e9418be106068f81f
hien <hiennguyennhu2004@gmail.com>
2024-11-25 20:29:25 +0700

    sửa lại giao diện của chọn hàng 1 chút

--- frontend/src/pages/Admin/orders/SearchProducts.jsx
+++ frontend/src/pages/Admin/orders/SearchProducts.jsx
@@ -130,11 +130,11 @@ const SearchProducts=({onStateChange})=>{
                 {dataProducts?.map((product, index) => (
                   <li
                     key={index}
-                    className="flex items-center w-full gap-3 p-2 cursor-pointer hover:bg-gray-200"
+                    className="flex items-center justify-between w-full gap-3 p-2 cursor-pointer hover:bg-gray-200"
                     onClick={()=>handleChooseProduct(product)}
                   >
                     <span>
-                      <svg
+                      {/* <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -147,12 +147,17 @@ const SearchProducts=({onStateChange})=>{
                           strokeLinejoin="round"
                           d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                         />
-                      </svg>
+                      </svg> */}
+                      <img className="max-w-16" src={product.image_path}/>
                     </span>
                     {product.name}
                     <br />
                     {product.id}
-                    <div className="ml-auto text-right">Số lượng tồn kho:{product.quantity}</div>
+                    <div className="ml-auto text-right">
+                      Giá tiền: {product.price}đ
+                      <br/>
+                      <p className="text-gray-400">Tồn kho:{product.quantity}</p>
+                    </div>
                   </li>
                 ))}
                 </ul>
